511474e4c4a600f2c5620963fe4d8aa6ff340025
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-11-15 11:05:09 +0530

    Create express-server.js

---
+++ express-server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const redis_client = require("./redis-client");
+app.get("/", async (req, res) => {
+  const cache_value = await redis_client.get("msg:1");
+  if (cache_value) {
+    res.send(cache_value);
+  } else {
+    redis_client.set("msg:1", "Hello, World!");
+    res.send("Hello, World!");
+  }
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
