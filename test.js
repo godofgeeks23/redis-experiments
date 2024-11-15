24021f0d1250b39c023896eb44c89faf340b2478
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-11-15 11:05:07 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const redis_client = require("./redis-client");
+
+async function init() {
+  await redis_client.set("msg:1", "Hello, World!");
+  const result = await redis_client.get("msg:1");
+  console.log(result);
+  redis_client.quit();
+}
+init();
