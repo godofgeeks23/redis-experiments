be2f601bdcb6204f63e777d3623f673755755550
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-11-18 23:12:07 +0530

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const redis_client = require("./redis-client");
+
 app.get("/", async (req, res) => {
   const cache_value = await redis_client.get("msg:1");
   if (cache_value) {
@@ -10,6 +11,7 @@ app.get("/", async (req, res) => {
     res.send("Hello, World!");
   }
 });
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
