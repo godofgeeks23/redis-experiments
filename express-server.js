f3607be2d78ca8bdee3d28451a742e7c9b830593
Aviral <aviralji4@gmail.com>
2025-05-11 22:23:58 +0530

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const redis_client = require("./redis-client");
 
+
 app.get("/", async (req, res) => {
+  // get value of key "msg:1" from redis
   const cache_value = await redis_client.get("msg:1");
   if (cache_value) {
     res.send(cache_value);
